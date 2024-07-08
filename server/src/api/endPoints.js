10a0b6f01e46996e806db054f697de091f6ff685
Kevin <dog.lion2298@outlook.com>
2024-07-08 10:26:40 -0500

    git commit -m "Entorno virtual"

--- server/src/api/endPoints.js
+++ server/src/api/endPoints.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {ping } = require('../controllers/pingController');
+const {login} = require('../controllers/loginController');
 
 router.get('/ping',ping);
 
+router.post('/Login', login);
+
+
 module.exports = router;
+
---
+++ server/src/controllers/loginController.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+module.exports.login = (req, res) =>{
+    constuser = req.body;
+    console.log(user);
+}
